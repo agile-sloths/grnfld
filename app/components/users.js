70b1875cb9febd64ec1bb849e84f0d3de1c3ef4a
Sean Quillen <sean.r.quillen@gmail.com>
2018-03-10 06:28:03 -0600

    adds in styling changes

--- app/components/users.js
+++ app/components/users.js
@@ -1,8 +1,15 @@
 angular.module('app')
-.controller('UsersCtrl', function($scope, $rootScope, $location, usersService) {
-  $scope.getGithubPicture = function() {
-  	console.log('Got it!');
-  }
+.controller('UsersCtrl', function($http, $scope, $rootScope, $location, usersService) {
+//   $scope.getGithubPicture = function(githubHandle) {
+//   	$http.get(`https://api.github.com/users/${githubHandle}`)
+//   	.then(function (response) {
+//   	  return response.avatar_url
+//   	})
+// 	  .catch(function (error) {
+// 	  	console.log(error);
+// 	  	// return default picture tag
+// 	  });
+//   }
 })
 .directive('users', function() {
   return {
